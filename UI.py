9e2d9406fd953269c0e8f501fe92d29bfa2992ce
anderTheBridge <ander@thebridge.tech>
2020-11-02 19:58:41 +0100

    fallo args

--- UI.py
+++ UI.py
@@ -16,10 +16,10 @@ args.bookid = st.text_input("Código del libro:", value="")
 credentials = user + ":" + password
 
 parsed_cred = sb.SafariBooks.parse_cred(credentials)
-st.write(parsed_cred)
+#st.write(parsed_cred)
 if not parsed_cred:
     st.write("Mal metido " + user + " o contraseña: " + password)
 else:
     args.cred = parsed_cred
 
-sb.SafariBooks(args)
+#sb.SafariBooks(args)
