79051860b967158e6cf6f040afa66ed6a7e284c3
anderTheBridge <ander@thebridge.tech>
2020-11-02 20:28:46 +0100

    st errors

--- safaribooks.py
+++ safaribooks.py
@@ -126,7 +126,7 @@ class Display:
         self.out(output)
 
         self.save_last_request()
-        #sys.exit(1)
+        sys.exit(1)
         st.stop()
     def unhandled_exception(self, _, o, tb):
         self.log("".join(traceback.format_tb(tb)))
