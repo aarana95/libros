d3ffdcfc389d61cdede5baed788ee3ce8ce400c6
anderTheBridge <ander@thebridge.tech>
2020-11-02 18:50:20 +0100

    interfaz

---
+++ UI.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import maze
+
+st.title('Estamos probando')
+user = st.text_input("Usuario:", value="")
+password = st.text_input("Contraseña:", value="", type="password")
+code = st.text_input("Código del libro:", value="")
