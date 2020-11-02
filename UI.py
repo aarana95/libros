9da7824d10dd42b34c6f7fb1e244fbec8dd4745a
anderTheBridge <ander@thebridge.tech>
2020-11-02 19:11:58 +0100

    activamos credenciales

--- UI.py
+++ UI.py
@@ -2,8 +2,17 @@ import streamlit as st
 #import cv2
 #import matplotlib.pyplot as plt
 #import numpy as np
+import safaribooks as sb
 
 st.title('Estamos probando')
 user = st.text_input("Usuario:", value="")
 password = st.text_input("Contraseña:", value="", type="password")
 code = st.text_input("Código del libro:", value="")
+
+
+credentials = user + ":" + password
+
+parsed_cred = sb.SafariBooks.parse_cred(credentials)
+
+if not parsed_cred:
+    st.write("Mal metido " + user + " o contraseña: " + password)
