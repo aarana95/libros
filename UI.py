a92f7b002a894cbcf5be85d4e5dbf1c8eda316e0
anderTheBridge <ander@thebridge.tech>
2020-11-02 19:40:43 +0100

    activamos credenciales

--- UI.py
+++ UI.py
@@ -13,6 +13,8 @@ code = st.text_input("Código del libro:", value="")
 credentials = user + ":" + password
 
 parsed_cred = sb.SafariBooks.parse_cred(credentials)
-
+st.write(parsed_cred)
 if not parsed_cred:
     st.write("Mal metido " + user + " o contraseña: " + password)
+
+@SafariBooks(args_parsed)
