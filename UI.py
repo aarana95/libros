3ecf19390463830337f6d2e9362a56632b6d188d
anderTheBridge <ander@thebridge.tech>
2020-11-06 13:46:45 +0100

    probando

--- UI.py
+++ UI.py
@@ -30,7 +30,7 @@ def main(args):
         """
         csv = df.to_csv(index=False)
         b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-        href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+        href = f'<a href="data:file/epub;base64,{b64}">Download csv file</a>'
         st.write(href)
         return href
 
